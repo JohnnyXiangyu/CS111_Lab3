e018b3edf125ebb8d62e99bb321603a72000d9f4
Johnny Wan <johnnywon@g.ucla.edu>
2020-03-01 15:33:22 -0800

    implement main(), fixed some signedness issues

--- A/lab3a.h
+++ A/lab3a.h
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include "ext2_fs.h"
+
 #define SUPERBLOCK_OFFSET 1024 /* superblock always starts at 1024 */
 
+/* structure for directory entry,
+ * but without name field so length is statically 8
+ */
 struct dir_entry_no_name {
 	__u32	inode;			/* Inode number */
 	__u16	rec_len;		/* Directory entry length */
@@ -10,27 +15,41 @@ struct dir_entry_no_name {
 	__u8	file_type;		/* file type */
 };
 
-
 /* utility function, calculate block offset given block number */
 unsigned int getBlockOffst(unsigned int block_id);
 
 /* utility function, calculate block size through information provided by superblock */
 void getBlockSize();
 
-/* read superblock and output superblock summary */
+/* read superblock and output superblock summary
+ * the FIRST to be called in main()
+ */
 void readSuperBlock();
 
-/* process group table, output group summary, then call readInodeInfo() and readBlockInfo() */
+/* process group table, output group summary
+ * included function calls: readInodeInfo()
+ *                          readBlockInfo()
+ * called ONLY in main()
+ */
 void readGroupDescriptor();
 
-/* process inode table, output inodes' information, inode summary, file types and indirect blocks' info  */
-void readInodeInfo(unsigned int group_num, struct ext2_group_desc cur_group);
+/* process inode table, output inodes' information, inode summary, file types and indirect blocks' info
+ * param: group_id group id of target group
+ *        cur_group group descriptor strcuture of target group 
+ * called ONLY in readGroupDescriptor()
+ */
+void readInodeInfo(unsigned int group_id, struct ext2_group_desc cur_group);
 
-/* process block bitmat, output free block information */
-void readBlockInfo(unsigned int group_num, struct ext2_group_desc cur_group);
+/* process block bitmat, output free block information
+ * param: group_id group id of target group
+ *        cur_group group descriptor structure of target group
+ * called ONLY in readGroupDescriptor()
+ */
+void readBlockInfo(unsigned int group_id, struct ext2_group_desc cur_group);
 
 /* process each dir_entry, output dir's parent inode number, logical byte offset, inode num of referenced file, entry length, name length and name
  * param: parent_inode inode number of parent inode
  *        block_id block id of current directory entry
+ * called ONLY in readInodeInfo()
  */
 void readDirectories(unsigned int parent_inode, unsigned int block_id);
