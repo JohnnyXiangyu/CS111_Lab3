a3945e544692d02d18fa694c67fcfdb13f244624
Johnny Wan <wonjohnny@outlook.com>
2020-03-10 22:26:13 -0700

    there's actually a case error in test script

--- B/sanity_tests.sh
+++ B/sanity_tests.sh
@@ -1,7 +1,7 @@
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22
 do
     ./lab3b test_data/P3B-test_$i.csv > test_result/test_result_$i.log
-    d=$(diff test_result/test_result_$i.log test_result/P3b-test_$i.err)
+    d=$(diff test_result/test_result_$i.log test_result/P3B-test_$i.err)
     if [ $? -ne 0 ]; then
         echo "test $i different from benchmark"
     fi
